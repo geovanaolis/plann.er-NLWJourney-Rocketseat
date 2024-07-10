993773dc8eb59463a8e72a11516ed2611e49449f
Geovana Oliveira da Silva <geovanaolis@outlook.com>
2024-07-10 16:35:01 -0300

    project-config

---
+++ mobile/src/app/index.tsx
@@ -0,0 +1,9 @@
+import {View, Text} from "react-native"
+
+export default function Index(){
+    return (
+        <View className="">
+            <Text> Hello World! </Text>
+        </View>
+    )
+}
