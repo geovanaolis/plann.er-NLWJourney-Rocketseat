9b7b65b2bc554c78d505729bd4e5255e6bdf98e3
Geovana Oliveira da Silva <geovanaolis@outlook.com>
2024-07-11 14:39:15 -0300

    some-fixes

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { Input } from "@/components/input"
 import { View, Text, Image } from "react-native"
 import { MapPin, Calendar as IconCalendar, Settings2, UserRoundPlus, ArrowRight } from "lucide-react-native"
@@ -5,13 +6,31 @@ import { colors } from "@/styles/colors"
 import { Button } from "@/components/button"
 
 
+enum StepForm {
+    TRIP_DETAILS = 1,
+    ADD_EMAIL = 2
+}
+
 export default function Index(){
+    const [stepForm, setStepForm] = useState(StepForm.TRIP_DETAILS)
+
+    function handleNextStepForm(){
+        if(stepForm === StepForm.TRIP_DETAILS){
+            setStepForm(StepForm.ADD_EMAIL)
+        }
+    }
+
     return (
         <View className="flex-1 items-center justify-center px-5">
             <Image 
-                source={require( "@/assets/logo.png")}
+                source={require( "@/assets/logo.png" )}
                 className="h-8"
                 resizeMode="contain"
+            />
+
+            <Image
+                source={require( "@/assets/bg.png" )}
+                className="absolute"
             />
 
             <Text className="text-zinc-400 font-regular text-center text-lg mt-3"> 
@@ -21,29 +40,47 @@ export default function Index(){
             <View className="w-full bg-zinc-900 p-4 rounded-xl my-8 border border-zinc-800">
                 <Input>
                     <MapPin color={colors.zinc[400]} size={20} />
-                    <Input.Field placeholder="Para onde?"/>
+                    <Input.Field 
+                        placeholder="Para onde?" 
+                        editable={stepForm === StepForm.TRIP_DETAILS}
+                    />
                 </Input>
 
                 <Input>
                     <IconCalendar color={colors.zinc[400]} size={20} />
-                    <Input.Field placeholder="Quando?"/>
+                    <Input.Field 
+                        placeholder="Quando?" 
+                        editable={stepForm === StepForm.TRIP_DETAILS}
+                    />
                 </Input>
 
-                <View className="border-b py-3 border-zinc-800">
-                    <Button variant="secondary">
-                        <Button.Title> Alterar local/data </Button.Title>
-                        <Settings2 color={colors.zinc[200]} size={20} />
-                    </Button>
-                </View>
+                { stepForm === StepForm.ADD_EMAIL && (
+                <>
+                    <View className="border-b py-3 border-zinc-800">
+                        <Button 
+                            variant="secondary" 
+                            onPress={() => setStepForm(StepForm.TRIP_DETAILS)}
+                        >
+                            <Button.Title> Alterar local/data </Button.Title>
+                            <Settings2 color={colors.zinc[200]} size={20} />
+                        </Button>
+                    </View>
 
-                <Input>
-                    <UserRoundPlus color={colors.zinc[400]} size={20} />
-                    <Input.Field placeholder="Quem estará na viagem?"/>
-                </Input>
+                    <Input>
+                        <UserRoundPlus color={colors.zinc[400]} size={20} />
+                        <Input.Field placeholder="Quem estará na viagem?"/>
+                    </Input>
+                </>
+                )}
 
-                <Button>
-                        <Button.Title> Continuar </Button.Title>
-                        <ArrowRight color={colors.lime[950]} size={20} />
+                <Button onPress={handleNextStepForm}>
+                    <Button.Title> 
+                        {
+                            stepForm === StepForm.TRIP_DETAILS ?
+                            "Continuar" : "Confirmar viagem"
+                        } 
+                    </Button.Title>
+                    <ArrowRight color={colors.lime[950]} size={20} />
                 </Button>
             </View>
 
